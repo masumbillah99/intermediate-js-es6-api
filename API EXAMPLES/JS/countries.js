d2ed29f16f3141cabaa9bf418612db719452f10f
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-02-24 15:14:23 +0600

    country details load by api done

--- API EXAMPLES/JS/countries.js
+++ API EXAMPLES/JS/countries.js
@@ -1,7 +1,7 @@
 const loadCountries = () => {
     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => displayCountries(data))
+        .then(data => displayCountries(data));
 }
 
 const displayCountries = countries => {
@@ -11,7 +11,7 @@ const displayCountries = countries => {
     //     console.log(country);
     // }
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country.cca2);
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML = `
@@ -19,9 +19,28 @@ const displayCountries = countries => {
             <p>Capital City: 
                 ${country.capital ? country.capital[0] : "No Capital City"}
             </p>
+            <button onclick="loadCountriesDetails('${country.cca2}')">Details</button>
         `;
         countriesContainer.appendChild(countryDiv);
     });
+}
+
+const loadCountriesDetails = (code) => {
+    // https://restcountries.com/v3.1/alpha/{code}
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showCountryDetail(data[0]));
+}
+
+const showCountryDetail = country => {
+    console.log(country);
+    const detailContainer = document.getElementById('country-detail');
+    detailContainer.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <img src="${country.flags.png}" alt="">
+    `;
 }
 
 loadCountries();
---
+++ API EXAMPLES/JS/meals.js
@@ -0,0 +1,7 @@
+const loadMeals = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+loadMeals();

95a32accf00701b29a27af45b1db8d11975790b2
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-02-24 16:24:03 +0600

    load data in bootstrap modal by api

--- API EXAMPLES/JS/meals.js
+++ API EXAMPLES/JS/meals.js
@@ -1,7 +1,60 @@
-const loadMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals = (searchText) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    // console.log(url);
+    fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => diplayMeals(data.meals))
 }
 
-loadMeals();
+// get container element
+// create child for each element
+// set content of the child element
+// append child
+const diplayMeals = meals => {
+    // console.log(meals);
+    const mealsConainer = document.getElementById("meals-container");
+    mealsConainer.innerHTML = '';
+    meals.forEach(meal => {
+        console.log(meal);
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        mealDiv.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="loading">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">This food teste is awesome. This food item load by calling API. And API is awesome for loading data from another source</p>
+                    <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                        Details
+                    </button>
+                </div>
+            </div>
+        `;
+        mealsConainer.appendChild(mealDiv);
+    });
+}
+
+const searchMeals = () => {
+    const searchText = document.getElementById('search-field').value;
+    // search meals
+    // console.log(searchText);
+    loadMeals(searchText);
+    searchText.value = '';
+}
+
+const loadMealDetail = idMeal => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = meal => {
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealsDetails = document.getElementById('mealDetailsBody');
+    mealsDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}" alt="">
+    `;
+}
+
+loadMeals('rice');

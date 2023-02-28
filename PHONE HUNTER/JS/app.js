2c94271777f55efd144e00134a0d51db5eb636ef
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-02-28 10:17:02 +0600

    dynamic api data load, enter button event & add a modal for showing phone details

--- PHONE HUNTER/JS/app.js
+++ PHONE HUNTER/JS/app.js
@@ -1,19 +1,24 @@
-
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 };
 
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     console.log(phones);
 
     const phonesCotainer = document.getElementById('phones-container');
     phonesCotainer.textContent = '';
 
-    // display 20 phones only
-    phones = phones.slice(0, 15);
+    // display 20 phones only (not a perfect way)
+    const showAll = document.getElementById('show-all');
+    if (dataLimit && phones.length > 11) {
+        phones = phones.slice(0, 11);
+        showAll.classList.remove('d-none');
+    } else {
+        showAll.classList.add('d-none');
+    }
 
     // dispaly no phones found
     const noPhone = document.getElementById('no-found-message');
@@ -29,11 +34,12 @@ const displayPhones = phones => {
         phoneCard.classList.add('col');
         phoneCard.innerHTML = `
             <div class="card py-3">
-                <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="">
+                <img src="${phone.image}" class="card-img-top w-50 mx-auto mb-3" alt="">
                 <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in
-                        to additional content. This content is a little bit longer.</p>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum ex consequatur aspernatur voluptatum. Sed, consectetur.
+                    </p>
+                    <button onclick="loadPhoneDetails('${phone.slug}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Details</button>
                 </div>
             </div>
         `;
@@ -44,20 +50,36 @@ const displayPhones = phones => {
     toggleSpinner(false);
 };
 
-// handler search button click
-document.getElementById('search-btn').addEventListener('click', () => {
+// search input field handler
+const processSearch = (dataLimit) => {
+    // start loader
     toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     if (searchText) {
-        loadPhones(searchText);
+        loadPhones(searchText, dataLimit);
     } else {
         alert(`Empty input field!!  Please write phone name`);
+        // stop loader
         toggleSpinner(false);
     }
-    searchField.value = '';
+    // searchField.value = '';
+}
+
+// handler search button click
+document.getElementById('search-btn').addEventListener('click', () => {
+    processSearch(11);
 });
 
+// search input field enter key event handler
+document.getElementById('search-field').addEventListener('keypress', function (event) {
+    // console.log(event.key);
+    if (event.key === "Enter") {
+        processSearch(11);
+    }
+});
+
+// load spinner function
 const toggleSpinner = (isLoading) => {
     const loaderSection = document.getElementById('loading-spinner');
     if (isLoading) {
@@ -67,5 +89,41 @@ const toggleSpinner = (isLoading) => {
     }
 }
 
+// not the best way to load show all
+document.getElementById('show-all-btn').addEventListener('click', () => {
+    processSearch();
+});
 
-// loadPhones();
+
+// load phone details
+const loadPhoneDetails = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+    console.log(data.data);
+}
+
+const displayPhoneDetails = (phone) => {
+    const phoneOther = phone.others;
+    const modalTitle = document.getElementById('phoneDetailModalLabel');
+    modalTitle.innerText = phone.name;
+    const phoneDetailsBody = document.getElementById('phone-details-body');
+    phoneDetailsBody.innerHTML = `
+        <div>
+            <p><b>Release Date:</b> ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
+            <p><b>WLAN:</b> ${phoneOther.WLAN}</p>
+            <p><b>Bluetooth:</b> ${phoneOther.Bluetooth}</p>
+            <h5>Main Features</h5>
+            <p><b>Chipset:</b> ${phone.mainFeatures.chipSet}</p>
+            <p><b>Size:</b> ${phone.mainFeatures.displaySize}</p>
+            <p><b>Memory:</b> ${phone.mainFeatures.memory}</p>
+            <p><b>Storage:</b> ${phone.mainFeatures.storage}</p>
+        </div>
+    `;
+
+    // <img src="${phone.image}" class="mx-auto w-50" alt="not found">
+}
+
+
+loadPhones('oppo');
